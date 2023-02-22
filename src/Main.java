dac82a0b01e307973c2df9ad725300111a3d30a6
Talita Hanna Cabral <talitalinden@gmail.com>
2023-02-22 14:19:35 -0300

    ex1java

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+//Elabore um programa que verifique se uma variável é maior, menor ou igual a outra e
+// exiba esta informação na tela.//
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 10;
+        int num3 = 15;
+
+        if (num1 > num2) {
+            System.out.println("É maior");
+        }else if (num1 == num2){
+            System.out.println("É igual");
+        }else{
+            System.out.println("É menor");
+
+            }
+        }
+        }
