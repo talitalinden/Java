cd19668b015c3d7ca3e19f5b809fe654dc303423
Talita Linden <106542991+talitalinden@users.noreply.github.com>
2023-04-27 16:29:53 -0300

    Delete Instrumentos directory

--- Instrumentos/src/InstrumentoSopro.java
+++
@@ -1,9 +0,0 @@
-public class InstrumentoSopro extends Instrumento {
-
-    public InstrumentosCorda(String nome, String material, int quantidade){
-        super (nome, material, quantidade);
-
-        public void afinar(){
-        System.out.println("Este instrumento afina.");
-    }
-}
